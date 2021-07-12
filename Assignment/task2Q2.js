e0f6b9f499952e5e75059b1c17b3050d7fc9360c
Shweta jha <85930909+shwetaj954@users.noreply.github.com>
2021-07-12 04:20:21 -0700

    Create task2Q2.js

---
+++ Assignment/task2Q2.js
@@ -0,0 +1,10 @@
+//Task 2 Q-(2)
+let stri="RaceCar";
+let race_str=stri.split("").reverse().join("");
+var check=stri===race_str?"This Sring is a panidrome.":"This is not a panidrome.";
+console.log(check);
+/*
+Output:
+
+This is not a panidrome.
+*/
